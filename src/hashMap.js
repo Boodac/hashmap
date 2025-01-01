278369635b2e576352e59e0eed344fccb25b8106
boodac <180095661+Boodac@users.noreply.github.com>
2025-01-01 10:46:26 -0500

    add hashmap.js

---
+++ src/hashMap.js
@@ -0,0 +1,14 @@
+import List from "../../linked-list/src/linkedlist.js";
+
+class HashMap {
+    #loadFactor;
+    #capacity;
+
+    hash(key) {
+
+    }
+
+    set(key, value) {
+        
+    }
+}
